70a0aefabd22f6d04d88348e368e8ce6b539cdf9
sambilokonga <abebawbegna@gmail.com>
2022-12-19 06:49:53 -0800

    function that divides element

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    _list = []
+    for i in range(list_length):
+        try:
+            d = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            d = 0
+            print("division by 0")
+        except TypeError:
+            d = 0
+            print("wrong type")
+        except IndexError:
+            d = 0
+            print("out of range")
+        finally:
+            _list.append(d)
+    return _list
