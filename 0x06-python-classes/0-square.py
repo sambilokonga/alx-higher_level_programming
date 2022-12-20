115310555244a2cbc293e0ea172d48bc3b90bf86
sambilokonga <abebawbegna@gmail.com>
2022-12-20 08:03:17 -0800

    empty class Square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Define a class for func Square"""
+
+class Square:
+    """Using empty class for Square"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+Python project on 0x06. Python - Classes and Objects
