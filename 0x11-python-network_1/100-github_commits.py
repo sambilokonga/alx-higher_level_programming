8c5eeee39f07e533eaa575b5306fcdf84a350c31
sambilokonga <abebawbegna@gmail.com>
2023-03-31 11:10:59 -0700

    Python script that takes 2 arguments

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""displays the value of the X-Request-Id variable found in
-the header of the response.
 """
-
-
+Python script to list 10 most recent commits
+to repository 'rails' by user 'rails'
+using GitHub API
+"""
 if __name__ == "__main__":
-    from requests import get
+    import requests
     from sys import argv
-
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
-    commits = get(url).json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
-
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        argv[2], argv[1])
+    response = requests.get(url)
+    json_list = response.json()
+    count = 0
+    for obj in json_list:
+        if count == 10:
+            break
+        print("{}: {}".format(
+            obj.get('sha'),
+            obj.get('commit').get('author').get('name')))
+        count = count + 1
