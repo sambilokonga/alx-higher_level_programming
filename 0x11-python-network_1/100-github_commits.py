341c3d8ed5094d876c595facea7e2a221cb662f0
sambilokonga <abebawbegna@gmail.com>
2023-03-31 11:18:46 -0700

    Python script that takes 2 arguments

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
 """
-Get the 10 most recent commits of the passed in repository
-of a given user, then display the sha and author name
-It's... apparently a Holberton School interview question
-"""
-import requests
 import sys
+import requests
 
 
-def get_commits():
-    req = requests.get("https://api.github.com/repos/{}/{}/commits"
-                       .format(sys.argv[2], sys.argv[1]))
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    for recent_commit in req.json()[:10]:
-        print("{}: {}".format(recent_commit.get('sha'),
-                              recent_commit.get('commit')
-                              .get('author').get('name')))
-
-
-if __name__ == '__main__':
-    get_commits()
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
