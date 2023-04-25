967e4934fc1209c42ab2b96c727283ad6105f5f8
sambilokonga <abebawbegna@gmail.com>
2023-04-25 07:14:15 -0700

    script that prints the title of a Star Wars movie

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,18 @@
 #!/usr/bin/node
 // script that displays the title of the Star Wars movie using API
 
+#!/usr/bin/node
+
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+
+request(API_URL + episodeNum, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
 });
